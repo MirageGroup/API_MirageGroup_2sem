ccbda10631360c8fd3796ec51e036517c423e304
VictorGuui <victorguilhermebranco@outlook.com>
2023-03-16 12:00:08 -0300

    Teste

--- src/main/java/api/App.java
+++ src/main/java/api/App.java
@@ -25,7 +25,10 @@ public class App extends Application {
     static void setRoot(String fxml) throws IOException {
         scene.setRoot(loadFXML(fxml));
     }
+     System.out.println("oi")
 
+
+     
     private static Parent loadFXML(String fxml) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
         return fxmlLoader.load();
