6708d85554d97d7d2e0a44f44489566eba63a387
VictorGuui <victorguilhermebranco@outlook.com>
2023-03-16 12:01:08 -0300

    Fix error

--- src/main/java/api/App.java
+++ src/main/java/api/App.java
@@ -25,10 +25,7 @@ public class App extends Application {
     static void setRoot(String fxml) throws IOException {
         scene.setRoot(loadFXML(fxml));
     }
-     System.out.println("oi")
 
-
-     
     private static Parent loadFXML(String fxml) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
         return fxmlLoader.load();
