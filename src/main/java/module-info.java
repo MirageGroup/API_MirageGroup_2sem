c82e8c471ff6e3384c7f468dde923a0681817b54
pedro11pucci <pedro11pucci@gmail.com>
2023-03-23 07:48:19 -0300

    arrumei as dependencias

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,9 @@
 module api {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.logging;
+    requires java.sql;
+    requires jakarta.persistence;
 
     opens api to javafx.fxml;
     exports api;
