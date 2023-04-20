302b46aa6acce75c5f191cd6a116ba2b4e7dcd3c
pedro11pucci <pedro11pucci@gmail.com>
2023-04-20 10:21:12 -0300

    feat: StudentDAO getById

--- src/main/java/dao/StudentDAO.java
+++ src/main/java/dao/StudentDAO.java
@@ -1,6 +1,7 @@
 package dao;
 
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import models.Student;
 
@@ -26,6 +27,24 @@ public class StudentDAO extends DAO {
             stmt.setInt(2, student.getId());
             stmt.execute();
             stmt.close();
+        }catch(SQLException e){
+            throw new RuntimeException(e);
+        }
+    }
+
+    public Student getById(int id){
+        String sql = "SELECT * FROM students WHERE id_student = ?";
+        try{
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            stmt.setInt(1, id);
+            ResultSet rs = stmt.executeQuery();
+            Student student = new Student();
+            while(rs.next()){
+                student.setId(rs.getInt("id_student"));
+                student.setName(rs.getString("name_student"));
+                student.setGrade(rs.getDouble("grade_student"));
+            }
+            return student;
         }catch(SQLException e){
             throw new RuntimeException(e);
         }
