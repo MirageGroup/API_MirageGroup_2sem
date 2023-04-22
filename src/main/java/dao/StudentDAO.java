fa6640fac91c8fd4bf7337eb2fd335c97d32ff73
VictorGuui <victorguilhermebranco@outlook.com>
2023-04-22 01:29:41 -0300

    Insert Students(MySQL)

--- src/main/java/dao/StudentDAO.java
+++ src/main/java/dao/StudentDAO.java
@@ -73,4 +73,16 @@ public class StudentDAO extends DAO {
         }
     }
 
+    public void insertStudent(Student student){
+        String sql = "INSERT INTO students(name_student) VALUES (?)";
+        try{
+            PreparedStatement stmt = conn.prepareStatement(sql);
+            stmt.setString(1, student.getName());
+            stmt.execute();
+            stmt.close();
+        }catch(SQLException e){
+            throw new RuntimeException(e);
+        }
+    }
+
 }
