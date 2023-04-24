9062ff32d87cd1931d9421944a5ce8cc0f5b0914
pedro11pucci <pedro11pucci@gmail.com>
2023-04-23 22:29:03 -0300

    fix

--- src/main/java/controllers/models/ClazzController.java
+++ src/main/java/controllers/models/ClazzController.java
@@ -2,7 +2,9 @@ package controllers.models;
 import java.util.ArrayList;
 
 import dao.ClazzDAO;
+import dao.StudentDAO;
 import models.Clazz;
+import models.Student;
 
 public class ClazzController {
 
@@ -20,18 +22,4 @@ public class ClazzController {
 
     }
 
-    public class StudentController{
-        public  String[] GetAllStudentsName(){
-            StudentDAO dao = new StudentDAO();
-            
-            ArrayList<Student> list = dao.getAll();
-            String students[] = new String[list.size()];
-
-            for(int i = 0; i < list.size(); i++){
-                students[i] = list.get(i).getName();
-            }
-
-            return students;
-        }
-    }
 }
