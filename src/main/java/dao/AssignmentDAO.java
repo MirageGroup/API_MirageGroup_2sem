387854a3167bd36ef4418093332fc6d0c0ed02d3
pedro11pucci <pedro11pucci@gmail.com>
2023-05-15 10:38:39 -0300

    cadastrando as atividades na sala

--- src/main/java/dao/AssignmentDAO.java
+++ src/main/java/dao/AssignmentDAO.java
@@ -3,20 +3,25 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import gui.ClienteGUI;
 import models.Assignment;
+import models.Clazz;
 
 
 public class AssignmentDAO extends DAO {
     
     public void save(Assignment assignment){
-        String sql = "INSERT INTO assignments(name_assignment, desc_assignment, date_assigned, date_due, max_grade) VALUES (?, ?, ?, ?, ?)";
+        ClazzDAO clazzdao = new ClazzDAO();
+        Clazz clazz = clazzdao.getByName(ClienteGUI.ComboSalas.getSelectedItem().toString());
+        String sql = "INSERT INTO assignments(fk_Classes_id_class ,name_assignment, desc_assignment, date_assigned, date_due, max_grade) VALUES (?, ?, ?, ?, ?, ?)";
         try{
             PreparedStatement stmt = conn.prepareStatement(sql, java.sql.Statement.RETURN_GENERATED_KEYS);
-            stmt.setString(1, assignment.getName());
-            stmt.setString(2, assignment.getDescription());
-            stmt.setString(3, assignment.getDate_assigned());
-            stmt.setString(4, assignment.getDate_due());
-            stmt.setDouble(5, assignment.getMax_grade());
+            stmt.setInt(1, clazz.getId());
+            stmt.setString(2, assignment.getName());
+            stmt.setString(3, assignment.getDescription());
+            stmt.setString(4, assignment.getDate_assigned());
+            stmt.setString(5, assignment.getDate_due());
+            stmt.setDouble(6, assignment.getMax_grade());
             stmt.execute();
             ResultSet rs = stmt.getGeneratedKeys();
             if(rs.next()){
