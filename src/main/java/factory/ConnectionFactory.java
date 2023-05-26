c4756e9ab2e87e0c83fa94239d5e8b3ccab421af
ThiagoCanedo <thiago.freitas17@outlook.com>
2023-05-25 22:00:48 -0300

    Adição de lembrete
    Adicionado area de para lembretes e adição de lembretes e reposicionamento de itens e paginas

--- src/main/java/factory/ConnectionFactory.java
+++ src/main/java/factory/ConnectionFactory.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class ConnectionFactory {
     public Connection getConnection(){
         try{
-            return DriverManager.getConnection("jdbc:mysql://localhost/api", "root", "fatec");
+            return DriverManager.getConnection("jdbc:mysql://localhost/api", "root", "123321");
         }catch(SQLException e){
             throw new RuntimeException(e);
         }
